0c4922fe23532228b8a7b1085251708e8e479ab4
Abdul Rafy <mrafybasra2020@gmail.com>
2024-09-06 12:30:16 +0500

    first commit

--- src/app/customer/components/AllProducts.js
+++ src/app/customer/components/AllProducts.js
@@ -67,7 +67,8 @@ const TopRatedProducts = () => {
   return (
     <div className="container mx-auto py-8">
       <h2 className="text-2xl font-semibold mb-6">Top Rated</h2>
-      <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 gap-6 auto-rows-fr">
+      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-4">
+
 
         {products.slice(0, visibleProducts).map((product) => {
           const originalPrice = calculateOriginalPrice(product.price, product.discount);
