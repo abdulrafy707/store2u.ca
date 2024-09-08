25256734f61ccb8c0558ea0b6e215ec3236dd263
Abdul Rafy <mrafybasra2020@gmail.com>
2024-09-08 15:37:18 +0500

    first commit

--- src/app/customer/components/Products.js
+++ src/app/customer/components/Products.js
@@ -1,10 +1,8 @@
 'use client';
 
-import React, { useEffect, useState, useRef } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useRouter } from 'next/navigation';
 import axios from 'axios';
-import { motion } from 'framer-motion';
-import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
 import { ThreeDots } from 'react-loader-spinner';
 
 const Products = () => {
@@ -12,7 +10,6 @@ const Products = () => {
   const [products, setProducts] = useState([]);
   const [subcategories, setSubcategories] = useState([]);
   const [loading, setLoading] = useState(true);
-  const productRefs = useRef([]);
   const router = useRouter();
 
   useEffect(() => {
@@ -41,24 +38,6 @@ const Products = () => {
 
   const handleProductClick = (id) => {
     router.push(`/customer/pages/products/${id}`);
-  };
-
-  const scrollLeft = (index) => {
-    if (productRefs.current[index]) {
-      productRefs.current[index].scrollBy({
-        left: -200, // Adjust scrolling distance as needed
-        behavior: 'smooth',
-      });
-    }
-  };
-
-  const scrollRight = (index) => {
-    if (productRefs.current[index]) {
-      productRefs.current[index].scrollBy({
-        left: 200, // Adjust scrolling distance as needed
-        behavior: 'smooth',
-      });
-    }
   };
 
   const calculateOriginalPrice = (price, discount) => {
@@ -105,8 +84,10 @@ const Products = () => {
               <h3 className="text-xl text-gray-800 font-normal mt-4 text-center md:text-left">
                 {category.name}
               </h3>
-              <div className="flex flex-col md:flex-row items-center md:items-start">
-                <div className="md:w-1/3 w-full md:pr-0 flex flex-col items-center md:items-start">
+
+              {/* Grid Layout */}
+              <div className="grid grid-cols-1 md:grid-cols-[1fr_2fr] gap-4 items-start">
+                <div className="category-image">
                   {category.imageUrl ? (
                     <img
                       src={`https://data.tascpa.ca/uploads/${category.imageUrl}`}
@@ -122,85 +103,69 @@ const Products = () => {
                     {category.description}
                   </p>
                 </div>
-                <div className="md:w-2/3 w-full relative flex items-center">
-                  <FiChevronLeft
-                    className="h-8 w-8 text-gray-500 cursor-pointer absolute left-0 top-1/2 transform -translate-y-1/2 z-10"
-                    onClick={() => scrollLeft(index)}
-                  />
-                  <div
-                    ref={(el) => (productRefs.current[index] = el)}
-                    className="flex space-x-4 md:space-x-2 overflow-hidden pl-0 w-full"
-                    style={{ scrollSnapType: 'x mandatory' }}
-                  >
-                    {categoryProducts.slice(0, 4).map((product) => {
-                      const originalPrice = calculateOriginalPrice(
-                        product.price,
-                        product.discount
-                      );
-                      return (
-                        <div
-                          key={product.id}
-                          className="bg-white shadow-md rounded-lg cursor-pointer border border-gray-300 relative h-[320px] md:h-[300px] w-[220px] md:w-[200px] flex-shrink-0"
-                          style={{ scrollSnapAlign: 'start' }}
-                        >
-                          {product.discount && (
-                            <div className="absolute z-40 top-2 right-2 bg-black text-white rounded-full h-8 w-8 flex items-center justify-center">
-                              -{product.discount}%
+
+                <div className="products-grid grid grid-cols-2 md:grid-cols-4 gap-4">
+                  {categoryProducts.slice(0, 4).map((product) => {
+                    const originalPrice = calculateOriginalPrice(
+                      product.price,
+                      product.discount
+                    );
+                    return (
+                      <div
+                        key={product.id}
+                        className="bg-white shadow-md rounded-lg cursor-pointer border border-gray-300 relative h-[320px] flex-shrink-0"
+                      >
+                        {product.discount && (
+                          <div className="absolute z-40 top-2 right-2 bg-black text-white rounded-full h-8 w-8 flex items-center justify-center">
+                            -{product.discount}%
+                          </div>
+                        )}
+                        <div className="relative">
+                          {product.images && product.images.length > 0 ? (
+                            <img
+                              src={`https://data.tascpa.ca/uploads/${product.images[0].url}`}
+                              alt={product.name}
+                              className="h-[240px] w-full object-cover mb-4 rounded bg-white"
+                              onClick={() => handleProductClick(product.id)}
+                            />
+                          ) : (
+                            <div className="h-[240px] w-full bg-gray-200 mb-4 rounded flex items-center justify-center text-gray-500">
+                              No Image
                             </div>
                           )}
-                          <div className="relative">
-                            {product.images && product.images.length > 0 ? (
-                              <motion.img
-                                src={`https://data.tascpa.ca/uploads/${product.images[0].url}`}
-                                alt={product.name}
-                                className="h-[240px] md:h-[220px] w-full object-cover mb-4 rounded bg-white"
-                                whileHover={{ scale: 1.1 }}
-                                transition={{ duration: 0.3 }}
-                                onClick={() => handleProductClick(product.id)}
-                              />
-                            ) : (
-                              <div className="h-[240px] md:h-[220px] w-full bg-gray-200 mb-4 rounded flex items-center justify-center text-gray-500">
-                                No Image
-                              </div>
-                            )}
-                            <button
-                              className="absolute bottom-2 right-2 bg-teal-500 text-white h-8 w-8 flex justify-center items-center rounded-full shadow-lg hover:bg-teal-600 transition-colors duration-300"
-                              onClick={() => handleProductClick(product.id)} // Navigate to product page
-                            >
-                              <span className="text-xl font-bold leading-none">+</span>
-                            </button>
-                          </div>
-                          <div className="px-2">
-                            <h3 className="text-sm font-normal text-gray-800 overflow-hidden text-ellipsis whitespace-nowrap">
-                              {product.name}
-                            </h3>
-                            <div className="grid grid-cols-2 px-0 py-2">
-                              <div className="flex items-center">
-                                {product.discount ? (
-                                  <div className="flex items-center justify-center  gap-3 flex-row-reverse">
-                                    <p className="text-xs font-normal text-gray-700 line-through ">
-                                      Rs.{product.price}
-                                    </p>
-                                    <p className="text-sm font-semibold text-red-700">
-                                      Rs.{originalPrice}
-                                    </p>
-                                  </div>
-                                ) : (
-                                  <p className="text-sm font-normal text-gray-700">
+                          <button
+                            className="absolute bottom-2 right-2 bg-teal-500 text-white h-8 w-8 flex justify-center items-center rounded-full shadow-lg hover:bg-teal-600 transition-colors duration-300"
+                            onClick={() => handleProductClick(product.id)}
+                          >
+                            <span className="text-xl font-bold leading-none">+</span>
+                          </button>
+                        </div>
+                        <div className="px-2">
+                          <h3 className="text-sm font-normal text-gray-800 overflow-hidden text-ellipsis whitespace-nowrap">
+                            {product.name}
+                          </h3>
+                          <div className="grid grid-cols-2 px-0 py-2">
+                            <div className="flex items-center">
+                              {product.discount ? (
+                                <div className="flex items-center justify-center  gap-3 flex-row-reverse">
+                                  <p className="text-xs font-normal text-gray-700 line-through ">
                                     Rs.{product.price}
                                   </p>
-                                )}
-                              </div>
+                                  <p className="text-sm font-semibold text-red-700">
+                                    Rs.{originalPrice}
+                                  </p>
+                                </div>
+                              ) : (
+                                <p className="text-sm font-normal text-gray-700">
+                                  Rs.{product.price}
+                                </p>
+                              )}
                             </div>
                           </div>
                         </div>
-                      );
-                    })}
-                  </div>
-                  <FiChevronRight
-                    className="h-8 w-8 text-gray-500 cursor-pointer absolute right-0 top-1/2 transform -translate-y-1/2 z-10"
-                    onClick={() => scrollRight(index)}
-                  />
+                      </div>
+                    );
+                  })}
                 </div>
               </div>
             </div>
